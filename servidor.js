9e9c226fab78d71ffdca5b797d4cb4ec8cfabd91
arbalrh <ak785202313@sina.com>
2021-02-22 17:19:47 +0800

    Desinstala o pacote body-parser por nao ser mais usado

--- servidor.js
+++ servidor.js
@@ -2,7 +2,6 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config({
     path: path.join(__dirname, ".env")
